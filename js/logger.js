5f51e9f1cfa35128dd78be27b546afd05c5c7262
bjsmithxyz <hello@bjsmith.xyz>
2025-09-25 21:23:24 +1000

    chore: deduplicate js/logger.js

--- js/logger.js
+++ js/logger.js
@@ -20,25 +20,3 @@ export const logger = {
     warn(...args) { console.warn(...args); },
     error(...args) { console.error(...args); }
 };
-import { CONFIG } from './config.js';
-
-/**
- * Small centralized logger that gates messages using CONFIG.DEBUG flags.
- * Usage:
- *  logger.debug('performance', 'message', obj)
- *  logger.info('startup message')
- */
-export const logger = {
-    debug(flag, ...args) {
-        try {
-            if (CONFIG && CONFIG.DEBUG && CONFIG.DEBUG[flag]) console.log(...args);
-        } catch (e) { /* swallow */ }
-    },
-    info(...args) {
-        try {
-            if (CONFIG && CONFIG.DEBUG && CONFIG.DEBUG.startup) console.log(...args);
-        } catch (e) {}
-    },
-    warn(...args) { console.warn(...args); },
-    error(...args) { console.error(...args); }
-};
